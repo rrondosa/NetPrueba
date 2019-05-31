974051c3b4f3dfc456608260cba0dee38d3bb5aa
rrondosa <1017.reyna@gmail.com>
2019-05-31 10:38:05 -0300

    proyecto de la calse 28-05 con la coneccion a db

---
+++ WebApplication7/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication7.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            //alta
+            dbEntities ctx = new dbEntities();
+            Empleado em = new Empleado();
+            em.apellido = "perez";
+            em.nombre = "Juan";
+            ctx.Empleado.Add(em);
+            ctx.SaveChanges();
+            
+            //modificar
+            int myid = 1;
+            //busco el objeto
+            Empleado em2 = ctx.Empleado.Find(myid);
+            em2.nombre = "Beto";
+            ctx.SaveChanges();
+            /*
+             * se puede consultar por su estado obj:
+             * -unchanged
+             * -added
+             * -modified
+             * -deleted
+             * -detached
+             */
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
